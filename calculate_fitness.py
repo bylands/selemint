5ea224fa285b633247871ec9ac21a5ba6e2bd5fb
bylands <17637688+bylands@users.noreply.github.com>
2023-03-08 06:22:03 +0100

    correct fitness calculation (each block contributes)

--- calculate_fitness.py
+++ calculate_fitness.py
@@ -6,6 +6,7 @@ import pandas as pd
 def calc_fitness(choices_df:pd.DataFrame, gene_df: pd.DataFrame, block_names, multipliers: list[float]) -> float:
 
     counter = [0, 0, 0]
+    fitness = 0
 
     for block in block_names:
         counter[0] = ((choices_df[f'{block}_first_choice'] == gene_df[f'{block}']) * choices_df['total_score']).sum()
@@ -17,5 +18,6 @@ def calc_fitness(choices_df:pd.DataFrame, gene_df: pd.DataFrame, block_names, mu
         for c, g, s in zip(choices_df[f'{block}_third_choices'], gene_df[f'{block}'], choices_df['total_score']):
             if g in c:
                 counter[2] += s
+    fitness += np.array([c * m for c, m in zip(counter, multipliers)]).sum()
 
-    return np.array([c * m for c, m in zip(counter, multipliers)]).sum()
+    return fitness
