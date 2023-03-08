correct fitness calculation (for real!)

calculate_fitness.py
@@ -18,6 +18,6 @@ def calc_fitness(choices_df:pd.DataFrame, gene_df: pd.DataFrame, block_names, mu
         for c, g, s in zip(choices_df[f'{block}_third_choices'], gene_df[f'{block}'], choices_df['total_score']):
             if g in c:
                 counter[2] += s
-    fitness += np.array([c * m for c, m in zip(counter, multipliers)]).sum()
+        fitness += np.array([c * m for c, m in zip(counter, multipliers)]).sum()
 
     return fitness