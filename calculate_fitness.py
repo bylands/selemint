a89a985b88602e712f43c1cda57aab03d41ceae2
bylands <17637688+bylands@users.noreply.github.com>
2023-03-12 15:53:16 +0100

    update fitness (limit to maximum places)

--- calculate_fitness.py
+++ calculate_fitness.py
@@ -3,12 +3,19 @@ import pandas as pd
 
 # calculate fitness
 
-def calc_fitness(choices_df:pd.DataFrame, gene_df: pd.DataFrame, block_names, multipliers: list[float]) -> float:
+def calc_fitness(choices_df:pd.DataFrame, gene_df: pd.DataFrame, blocks, block_names, multipliers: list[float]) -> float:
 
     counter = [0, 0, 0]
     fitness = 0
 
     for block in block_names:
+
+        counts = pd.DataFrame(gene_df[block].value_counts().sort_index())
+        max_counts = pd.DataFrame({block: blocks[block].values()}, index=blocks[block].keys()).sort_index()
+
+        if (counts[block] > max_counts[block]).any():
+            return 0
+
 
         for i, level in enumerate(['first', 'seconds', 'thirds']):
             filter_col = [col for col in choices_df if col.startswith(f'{block}_{level}')]
--- crossover.py
+++ crossover.py
@@ -18,21 +18,25 @@ def get_offspring(pool: dict, choices_fd: pd.DataFrame, blocks, n_genes: int, mu
 
     for i in range(n_genes):
         id = f'GID{i:05}'
-        key1, key2 = random.sample(pool_keys, k = 2)
-        off1, off2 = pool[key1], pool[key2]
 
-        pos = rng.integers(n_people)
-        off_fd1 = off1[0][:pos]
-        off_fd2 = off2[0][pos:]
+        while True:
+            key1, key2 = random.sample(pool_keys, k = 2)
+            off1, off2 = pool[key1], pool[key2]
 
-        off_fd = pd.concat([off_fd1, off_fd2])
+            pos = rng.integers(n_people)
+            off_fd1 = off1[0][:pos]
+            off_fd2 = off2[0][pos:]
 
-        for block in blocks:
-            options = list(blocks[block].keys())
-            random_value = rng.random(size=n_people)
-            off_fd[f'{block}'] = (np.where(random_value < rate, random.choice(options), off_fd[f'{block}']))
-        
-        fitness = calculate_fitness.calc_fitness(choices_fd, off_fd, block_names, multipliers)
+            off_fd = pd.concat([off_fd1, off_fd2])
+
+            for block in blocks:
+                options = list(blocks[block].keys())
+                random_value = rng.random(size=n_people)
+                off_fd[f'{block}'] = (np.where(random_value < rate, random.choice(options), off_fd[f'{block}']))
+            
+            fitness = calculate_fitness.calc_fitness(choices_fd, off_fd, blocks, block_names, multipliers)
+            if fitness > 0:
+                break
 
         offsprings[id] = [off_fd, fitness]
 
